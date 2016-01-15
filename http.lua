4a5d9f98213787faadd1972da9838f1ab82a06af
Pisces000221 <1786762946@qq.com>
2016-01-15 22:40:46 +0800

    登录部分 完成√

--- http.lua
+++ http.lua
@@ -1,25 +1,26 @@
 http = {}
+http.cookie_jar = 'cookies.txt'
 
 function http.get(url)
-    local handle = io.popen('curl -q -k -s -X GET '
+    local handle = io.popen('curl -q -k -s -b ' .. http.cookie_jar .. ' -c ' .. http.cookie_jar .. ' -X GET '
         .. '-e "http://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2" '
         .. '-m 10 "' .. url .. '"', 'r')
-    local result = handle:read('*a')
+    local response = handle:read('*a')
     handle:close()
-    return result
+    return response
 end
 
 function http.post(url, content)
-    local handle = io.popen('curl -q -k -s -X POST '
+    local handle = io.popen('curl -q -k -s -b ' .. http.cookie_jar .. ' -c ' .. http.cookie_jar .. ' -X POST '
         .. '-e "http://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2" '
         .. '-m 10 "' .. url .. '" -d "' .. string.gsub(content, '"', '\\\"') .. '"', 'r')
-    local result = handle:read('*a')
+    local response = handle:read('*a')
     handle:close()
-    return result
+    return response
 end
 
 function http.download(url, path)
-    local handle = io.popen('curl -q -k -s -X GET '
+    local handle = io.popen('curl -q -k -s -b ' .. http.cookie_jar .. ' -c ' .. http.cookie_jar .. ' -X GET '
         .. '-e "http://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2" '
         .. '-o "' .. path .. '" "' .. url .. '"', 'r')
     handle:close()
--- webqq.lua
+++ webqq.lua
@@ -42,16 +42,52 @@ function webqq.try_login(self)
 end
 
 function webqq.retrieve_qrcode(self)
+    print('Bring your phone. You\'ll need it ;)')
     -- 取得 Smart QQ 页面的真实内容（w.qq.com 上有一个 iframe）
     local entry_html = http.get('http://w.qq.com/login.html')
     -- 结尾是 "... &f_qr=" + f_qr，窝们把 f_qr = '0' 塞进去
     local redirect_url = string.match(entry_html, '%.src = "(.-)"') .. '0'
     local real_content = http.get(redirect_url)
     local appid = string.match(real_content, '<input type="hidden" name="aid" value="(%d-)"')
-    -- 下载二维码
-    http.download('https://ssl.ptlogin2.qq.com/ptqrshow?appid=' .. appid .. '&e=0&l=L&s=8&d=72&v=4', 'login.jpg')
-    -- TODO: 增加其他系统的支持 (http://stackoverflow.com/questions/264395/)
-    os.execute('open login.jpg')
+    -- 三个确认二维码状态时用到的参数
+    local pwsecur_css = string.match(real_content, 'g_mibao_css=encodeURIComponent%("(.-)"%)')
+    local js_ver = string.match(real_content, 'g_pt_version=encodeURIComponent%("(.-)"%)')
+    local login_sig = string.match(real_content, 'g_login_sig=encodeURIComponent%("(.-)"%)')
+
+    local attempts = 0
+    while attempts < 10 do
+        local start_time = os.time()
+        -- 下载二维码
+        http.download('https://ssl.ptlogin2.qq.com/ptqrshow?appid=' .. appid .. '&e=0&l=L&s=8&d=72&v=4', 'login.jpg')
+        -- TODO: 增加其他系统的支持 (http://stackoverflow.com/questions/264395/)
+        print('Scan the QR code (login.jpg) with QQ for Mobile to finish the log-in process')
+        --os.execute('open login.jpg')
+
+        -- 66: 未失效；67: 验证中；65: 失效；0: 验证完成
+        local status = 66, status_text
+        local verifying_message_shown = false
+        while status ~= 0 and status ~= 65 do
+            -- 循环检查二维码状态
+            status_text = http.get('https://ssl.ptlogin2.qq.com/ptqrlogin?webqq_type=10&remember_uin=1&login2qq=1&aid=' .. appid
+                .. '&u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10&ptredirect=0&ptlang=2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert&action=0-0-' .. tostring((os.time() - start_time) * 1000)
+                .. '&mibao_css=' .. pwsecur_css .. '&t=undefined&g=1&js_type=0&js_ver=' .. js_ver .. '&login_sig=' .. login_sig)
+            -- status_text: ptuiCB('66','0','','0','二维码未失效。(3616543552)', '');
+            status = tonumber(string.match(status_text, "ptuiCB%('(%d-)'"))
+            if status == 67 and not verifying_message_shown then
+                verifying_message_shown = true
+                print('Nearly there! I can see you\'re holding your phone ←_←')
+            end
+        end
+        if status == 0 then
+            print('QR code verified! Just a few more seconds!')
+            local one_more_url = string.match(status_text, "ptuiCB%('%d-',.-'%d-',.-'(.-)'")
+            http.get(one_more_url)
+            break
+        else
+            print('QR code expired. Getting another...')
+        end
+        attempts = attempts + 1
+    end
 end
 
 function webqq.login(self)
@@ -66,4 +102,18 @@ function webqq.login(self)
             end
         end
     end
+    print('Log in successful! （≧∇≦）')
+    local info_resp = json:decode(http.get('http://s.web2.qq.com/api/get_self_info2'))
+    if info_resp['retcode'] ~= 0 then
+        print('Failed to retrieve account information... I don\'t know what to do either >^<')
+        return false
+    end
+    local my_info = info_resp['result']
+    print("Hello. I'm " .. my_info['nick'] .. ' (' .. tostring(my_info['uin']) .. ')')
+    local today = os.date('%Y %m %d')
+    local y, m, d = string.match(today, '(%d-) (%d-) (%d*)')
+    if m == my_info['birthday']['month'] and d == my_info['birthday']['day'] then
+        print('Happy birthday ' .. tostring(y - my_info['birthday']['year']) .. '-year-old ' .. my_info['nick'] .. '!! ☆*:.｡. o(≧▽≦)o .｡.:*☆')
+    end
+    return true
 end
