64e1dad962ca8cf170c28822e0b130f52d0a19f5
Pisces000221 <1786762946@qq.com>
2016-01-17 01:51:23 +0800

    加入zzz函数（其实就是sleep啦= =）

---
+++ compiled/zzz-posix.c
@@ -0,0 +1,19 @@
+// http://www.troubleshooters.com/codecorn/lua/lua_lua_calls_c.htm#_Make_an_msleep_Function
+// gcc -Wall -shared -fPIC -o zzz.so -llua zzz-posix.c
+#include <unistd.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+static int zzz(lua_State *L)
+{
+    long msec = lua_tointeger(L, -1);
+    sleep(msec);
+    return 0;
+}
+
+int luaopen_zzz(lua_State *L)
+{
+    lua_register(L, "zzz", zzz);
+    return 0;
+}
--- main.lua
+++ main.lua
@@ -1,5 +1,9 @@
+require 'zzz'
 require './webqq'
 
 local bot = webqq:create()
 bot:login()
-while true do bot:check_message() end
+while true do
+    -- 里面调用的cURL会自动等待直到收到消息。。所以不用zzz
+    bot:check_message()
+end
