01775b8f53ef3de4b93ae752b5ef8e64356db984
Pisces000221 <1786762946@qq.com>
2016-01-19 13:14:48 +0800

    修改 dot_counter

--- ai/dot_counter.lua
+++ ai/dot_counter.lua
@@ -1,4 +1,4 @@
-ai.init_storage.dot_counter = { total = 0 }
+ai.init_storage.dot_counter = { total = 0, members = {} }
 
 ai.register_handler(function (self, uin, messages)
     for i = 1, #messages do
@@ -9,5 +9,8 @@ ai.register_handler(function (self, uin, messages)
     return false
 end, function (self, uin, messages)
     self.ai_storage.dot_counter.total = self.ai_storage.dot_counter.total + 1
-    self:send_message(string.rep('。', self.ai_storage.dot_counter.total))
+    self.ai_storage.dot_counter.members[uin] = (self.ai_storage.dot_counter.members[uin] or 0) + 1
+    self:send_message('↑' .. self.members[uin]['card'] .. ' 的第'
+        .. self.ai_storage.dot_counter.members[uin] .. '个豆豆，总共第' .. self.ai_storage.dot_counter.total
+        .. '个豆豆')
 end)
