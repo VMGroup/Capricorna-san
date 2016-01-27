8e9813bcdb7f38757e8e9a85fdf3b48c1d4b5f0a
Pisces000221 <1786762946@qq.com>
2016-01-27 23:13:53 +0800

    直接使用 OpemWeatherMap 提供的中文天气描述
    原来是可以提供中文的啊QAQ http://openweathermap.org/current#multi

--- ai/weather.lua
+++ ai/weather.lua
@@ -1,46 +1,8 @@
 json = json or require('./libs/JSON')
 require './http'
 
---local api_key = 'f6a3c950db757b5268a621dfc2ad4e7c'
 local api_key = '21b1550dccd8e2b660b7bed89deb2fd1'
---local api_key = '44db6a862fba0b067b1930da0d769e98'
 
--- http://openweathermap.org/weather-conditions
-local weather_desc = {
-    [500] = '小雨',
-    [501] = '中雨',
-    [502] = '大雨',
-    [503] = '炒鸡大雨',
-    [504] = '极限大雨QAQ',
-
-    [600] = '小雪',
-    [601] = '中雪',
-    [602] = '大雪',
-    [611] = '雨夹雪',
-    [612] = '大雨夹雪',
-    [615] = '小雨夹雪',
-    [616] = '雨夹雪',
-    [620] = '小型炒鸡大雪',
-    [621] = '中型炒鸡大雪',
-    [622] = '大型炒鸡大雪',
-
-    [701] = '薄雾',
-    [711] = '烟雾（窝也不知道是什么）',
-    [721] = '霾QAQ',
-    [731] = 'sand, dust whirls',
-    [741] = '雾',
-    [751] = '沙尘',
-    [761] = '沙尘',
-    [762] = '沙尘/火山灰',
-    [771] = '暴风',
-    [781] = '龙卷风',
-
-    [800] = '晴空万里',
-    [801] = '晴（少云）',
-    [802] = '疏云',
-    [803] = '多云',
-    [804] = '阴云'
-}
 -- 东南西北
 local dirs = {
     '北', '北-东北', '东北', '东北-东', '东', '东-东南', '东南', '东南-南',
@@ -51,7 +13,7 @@ local dir_desc = function (deg)
 end
 local weather_report = function (d)
     local s = d.orig_name .. '[' .. d.name .. '] 天气（' .. os.date('%Y-%m-%d %H:%M', d.dt) .. ' 更新）：\n'
-        .. (weather_desc[d.weather[1].id] or d.weather[1].description) .. ' 温度：' .. d.main.temp .. '℃；\n'
+        .. d.weather[1].description .. ' 温度：' .. d.main.temp .. '℃；\n'
         .. '气压：' .. d.main.pressure .. ' hPa；相对湿度：' .. d.main.humidity .. '%；\n'
         .. '风力：' .. d.wind.speed .. '级'
     if d.wind.deg then
@@ -67,7 +29,7 @@ local weather_forecast = function (d)
         cur = d.list[i]
         last_disp = cur.dt
         s = s .. os.date('%Y-%m-%d', last_disp) .. '：'
-            .. (weather_desc[cur.weather[1].id] or cur.weather[1].description) .. ' '
+            .. cur.weather[1].description .. ' '
             .. math.floor(cur.main.temp_min) .. '℃ ~ ' .. math.floor(cur.main.temp_max) .. '℃ 风力' .. cur.wind.speed .. '级\n'
     end end
     return s .. '数据来源：OpenWeatherMap'
@@ -95,10 +57,10 @@ ai.register_handler('weather',
             print('Retrieving weather data...')
             if is_forecast then
                 resp = json:decode(http.get(
-                    'http://api.openweathermap.org/data/2.5/forecast?q=' .. city_name .. '&units=metric&appid=' .. api_key))
+                    'http://api.openweathermap.org/data/2.5/forecast?q=' .. city_name .. '&units=metric&lang=zh_cn&appid=' .. api_key))
             else
                 resp = json:decode(http.get(
-                    'http://api.openweathermap.org/data/2.5/weather?q=' .. city_name .. '&units=metric&appid=' .. api_key))
+                    'http://api.openweathermap.org/data/2.5/weather?q=' .. city_name .. '&units=metric&lang=zh_cn&appid=' .. api_key))
             end
         end
         if tonumber(resp.cod) == 200 then
@@ -135,7 +97,7 @@ ai.register_handler('weather',
         while resp == nil do
             print('Retrieving weather data...')
             resp = json:decode(http.get(
-                'http://api.openweathermap.org/data/2.5/weather?q=' .. city_name .. '&units=metric&appid=' .. api_key))
+                'http://api.openweathermap.org/data/2.5/weather?q=' .. city_name .. '&units=metric&lang=zh_cn&appid=' .. api_key))
         end
         if tonumber(resp.cod) == 200 then
             resp.orig_name = city_name
