864914edb3b71f4516d09a9965902f2880de2b51
Pisces000221 <1786762946@qq.com>
2016-02-08 00:56:06 +0800

    更新一些资源的描述和匹配模式串

--- ai/welcomer.lua
+++ ai/welcomer.lua
@@ -39,20 +39,22 @@ ai.register_timer('welcomer',
 local downloads = {
     -- 正经的内容
     { name = 'v[34].+安装包', full_name = 'Vocaloid 3/4 安装包+中文音源（洛天依&言和&乐正绫&心华）', link = 'http://pan.baidu.com/s/1o6wzmBo 密码：rih3' },
-    { name = '音源', full_name = 'Vocaloid 3/4 所有音源（所有语种）', link = 'http://pan.baidu.com/s/1jGvl3j4 密码：yzy5' },
-    { name = 'fl.+破解', full_name = 'FL Studio 12 注册机', link = 'http://pan.baidu.com/s/1borvNLd 密码：kryu' },
-    { name = 'fl.+注册', full_name = 'FL Studio 12 注册机', link = 'http://pan.baidu.com/s/1borvNLd 密码：kryu' },
+    { name = '音源.+下载', full_name = 'Vocaloid 3/4 所有音源（所有语种）', link = 'http://pan.baidu.com/s/1jGvl3j4 密码：yzy5' },
+    { name = 'fl.+破解', full_name = 'FL Studio 11/12 注册机', link = 'http://pan.baidu.com/s/1borvNLd 密码：kryu' },
+    { name = 'fl.+注册', full_name = 'FL Studio 11/12 注册机', link = 'http://pan.baidu.com/s/1borvNLd 密码：kryu' },
     { name = 'fl.+安装包', full_name = 'FL Studio 12', link = 'http://pan.baidu.com/s/1hqT5dmW 密码：e7nj' },
-    { name = 'audacity', full_name = 'Audacity 2.1.1 for Windows', link = 'http://pan.baidu.com/s/1hqlhGzy 密码：4xtu' },
+    { name = 'audacity.+下载', full_name = 'Audacity 2.1.1 for Windows', link = 'http://pan.baidu.com/s/1hqlhGzy 密码：4xtu' },
     -- 有点不正经的内容
-    { name = '入戏太深', full_name = '马旭东《入戏太深》 Vocaloid全套资源', link = 'http://pan.baidu.com/s/1pJiOfUj' },
+    { name = '入戏太深', full_name = '马旭东《入戏太深》 谱&伴奏', link = 'http://pan.baidu.com/s/1pJiOfUj' },
     -- 非常不正经的内容
     { name = 'osu.+安装包', full_name = 'osu! 安装包', link = 'http://pan.baidu.com/s/1mgriaEW 密码：ya7j' },
-    { name = 'lantern', full_name = 'Lantern（墙内）下载页面', link = 'https://github.com/getlantern/lantern/releases' }
+    { name = 'lantern', full_name = 'Lantern（墙内）下载页面', link = 'https://github.com/getlantern/lantern/releases' },
+    { name = 'free pascal', full_name = 'Free Pascal 2.6.4 for OS X', link = 'http://pan.baidu.com/s/1i3leJtV 密码：mqdf' },
+    { name = '20151029', full_name = 'HSEFZ Practice Contest 2015.10.29', link = 'http://pan.baidu.com/s/1mgjHQIk' }
 }
 local downloads_msg = {
     '这里是 %s ➝ %s，拿走不谢～',
-    '窝这里刚好有哦。。%s | %s'
+    '窝这里有。。%s | %s'
 }
 ai.register_handler('welcomer',
     function () end,
