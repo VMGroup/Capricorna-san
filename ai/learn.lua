d4a4f9dbf83765d7664f2114b5144855c915fb2d
Pisces000221 <1786762946@qq.com>
2016-02-11 21:01:25 +0800

    对于学过的东西给出回答~

--- ai/learn.lua
+++ ai/learn.lua
@@ -7,6 +7,10 @@
 --   "capricorna，学习 / Trim me! /  Also trim me! "
 --   "  cap 学习。。/a/b/c/d/e"  (Should not crash)
 
+--   "cap Q"
+--   "capQuestion"  (问题不区分大小写)
+--   "cap  Quest     "
+
 local get_msg = {
     'get√',
     '哦。。そうか',
@@ -20,7 +24,8 @@ local told_msg = {
     {1, '%s。。说好的“%s”呢 ←_←'},
     {2, '不是“%s”嘛。。（%s说的）'},
     {3, '上次说的不是这样的。。。'},
-    {3, '窝不听窝不听＞＜'}
+    {3, '窝不听窝不听＞＜'},
+    {3, '不明觉厉ing…和说好的不一样QAQ'}
 }
 ai.register_handler('learn',
     function (self, storage)
@@ -28,27 +33,37 @@ ai.register_handler('learn',
 
     function (self, uin, message, storage)
         if message:lower():find('cap.*学习.*/%s*.-%s*/%s*.-%s*$') then return 1
-        else return 0 end
+        else
+            local question = message:lower():match('cap%s*(.-)%s*$')
+            if storage[question] then return 1
+            else return 0 end
+        end
     end,
 
     function (self, uin, message, storage)
         local qst, ans = message:match('学习.*/%s*(.-)%s*/%s*(.-)%s*$')
-        --print(qst .. '-' .. ans .. '.')   -- For debug use
-        qst = qst:lower()
-        local last = storage[qst]
-        if last then
-            local typ, fmt = ai.rand_from(told_msg)
-            if typ == 1 then
-                self:send_message(string.format(fmt, self.member_info[last.teacher].card, last.text))
-            elseif typ == 2 then
-                self:send_message(string.format(fmt, last.text, self.member_info[last.teacher].card))
+        if qst then
+            --print(qst .. '-' .. ans .. '.')   -- For debug use
+            qst = qst:lower()
+            local last = storage[qst]
+            if last then
+                local typ, fmt = ai.rand_from(told_msg)
+                if typ == 1 then
+                    self:send_message(string.format(fmt, self.member_info[last.teacher].card, last.text))
+                elseif typ == 2 then
+                    self:send_message(string.format(fmt, last.text, self.member_info[last.teacher].card))
+                else
+                    self:send_message(fmt)
+                end
             else
-                self:send_message(fmt)
+                -- 新增记录
+                storage[qst] = { teacher = uin, text = ans }
+                self:send_message(ai.rand_from(get_msg))
             end
         else
-            -- 新增记录
-            storage[qst] = { teacher = uin, text = ans }
-            self:send_message(ai.rand_from(get_msg))
+            -- 被问问题了。。
+            local question = message:lower():match('cap%s*(.-)%s*$')
+            self:send_message(storage[question].text)
         end
     end
 )
