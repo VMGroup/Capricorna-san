c3a6413387139f57f530a168d0fde5f00d13ecc8
Pisces000221 <1786762946@qq.com>
2016-02-12 17:13:53 +0800

    nominator和welcomer更正常的消息。。

--- ai/nominator_tail.lua
+++ ai/nominator_tail.lua
@@ -1,8 +1,27 @@
 local called_msg = {
     '嗯。。',
     '？',
-    '窝在这儿',
-    'Here~'
+    '。',
+    'Here',
+    '喜闻乐见',
+    '啊哈哈哈哈……你说什么？'
+}
+local asked_msg = {
+    '布吉岛',
+    '不知道。。不过泥可以告诉我啊。。',
+    'coder没讲过这事 = =',
+    '这个时候说“nil”一定没错←_←',
+    '虽然不明白你们在说什么但我觉得挠头一定是对的',
+    '（假装自己明白但是不说',
+    '。。今天天气真好',
+    '。。阿绫出来 问你个问题',
+    '作甚。。\nCap 学习/问题/应答\nCap Wiki 某某某'
+}
+local greet_msg = { -- 真·greeter?
+    'Hello',
+    'Hi',
+    '泥壕',
+    '窝是这里的AI之一。。请多指教ww'
 }
 
 ai.register_handler('nominator',
@@ -15,7 +34,13 @@ ai.register_handler('nominator',
         else return 0 end   -- 如果在前面任何一个模块中被处理过，这里的1将不会发挥作用。。
     end,
 
-    function (self)
-        self:send_message(ai.rand_from(called_msg))
+    function (self, uid, message)
+        message = message:lower()
+        if message:find('hello') or message:find('[^%w]?hi[^%w]?') or message:find('你好')
+            or message:find('泥嚎') or message:find('泥壕')
+        then self:send_message(ai.rand_from(greet_msg))
+        elseif message:find('什么') or message:find('谁') or message:find('啥') then
+            self:send_message(ai.rand_from(asked_msg))
+        else self:send_message(ai.rand_from(called_msg)) end
     end
 )
